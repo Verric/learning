1a5975a7f211f33d1b09d9c55ec73fc7fe62756c
verric <beijing-planes@hotmail.com>
2017-07-01 16:15:53 +1000

    added missing providers to test fixtures

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -2,13 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
 
+import { TodoDataService } from '../todo-data.service';
+
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      declarations: [ TodoComponent ],
+      providers: [TodoDataService]
     })
     .compileComponents();
   }));
