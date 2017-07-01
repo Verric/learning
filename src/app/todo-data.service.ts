386ae5972e9122a2320cb7bf31c294b0caa35ffb
verric <beijing-planes@hotmail.com>
2017-07-01 13:19:22 +1000

    created data service to store logic

---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { Todo } from './todo/todo';
+
+@Injectable()
+export class TodoDataService {
+
+  id: number = 0; //service will generate todo ids
+  todos: Todo[] = new Array<Todo>();
+
+  constructor() { }
+
+// queries
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+// actions
+  addTodo(todo: Todo): void {
+    todo.id = this.id++; 
+    this.todos.splice(0,0,todo); //ensures new items are at the "top" of the list
+  }
+
+  toggleComplete(id: number): void {
+    this.todos.filter(todo => todo.id === id).map(todo => todo.completed = !todo.completed);
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+}
